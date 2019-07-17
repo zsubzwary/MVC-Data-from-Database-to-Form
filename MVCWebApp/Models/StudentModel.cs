795675abb28182ffda98142d6e86144f93722f37
Zakawat <31027252+zsubzwary@users.noreply.github.com>
2019-07-17 15:51:16 +0500

    Added annotation to StudentModel

--- MVCWebApp/Models/StudentModel.cs
+++ MVCWebApp/Models/StudentModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,11 +8,32 @@ namespace MVCWebApp.Models
 {
     public class StudentModel
     {
+        [Required(ErrorMessage = "This field is required")]
+        [Display(Name = "Student ID")]
         public int StudentID { get; set; }
+
+        [Required(ErrorMessage = "This field is required")]
+        [Display(Name = "First Name")]
         public String FirstName { get; set; }
+
+        [Required(ErrorMessage = "This field is required")]
+        [Display(Name = "Student ID")]
         public String LastName { get; set; }
+
+        [Required(ErrorMessage = "This field is required")]
+        [Display(Name = "Student ID")]
+        [DataType(DataType.EmailAddress)]
         public String EmailAddress { get; set; }
+
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "This field is required")]
+        [Display(Name = "Student ID")]
         public String Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "This field is required")]
+        [Display(Name = "Student ID")]
+        [Compare("Password", ErrorMessage = "Both password should be same.")]
         public String ConfirmPassword { get; set; }
     }
 }
