a33f9adf4cdf7ecb90c4d0fc5f539e57d6902fda
Zakawat <31027252+zsubzwary@users.noreply.github.com>
2019-07-17 15:46:38 +0500

    Added StudentDisplay Model

---
+++ MVCWebApp/Models/StudentModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCWebApp.Models
+{
+    public class StudentModel
+    {
+        public int StudentID { get; set; }
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public String EmailAddress { get; set; }
+        public String Password { get; set; }
+        public String ConfirmPassword { get; set; }
+    }
+}
