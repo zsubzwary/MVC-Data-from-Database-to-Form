5479569f8c8d19be5eaf23f6a51935e8a02cd450
Zakawat <31027252+zsubzwary@users.noreply.github.com>
2019-07-18 11:08:00 +0500

    Added connection string for SQLite

--- MVCWebApp/HelperClasses/DBHelper.cs
+++ MVCWebApp/HelperClasses/DBHelper.cs
@@ -1,6 +1,7 @@
 ﻿using MVCWebApp.Models;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
@@ -10,13 +11,14 @@ namespace MVCWebApp.HelperClasses
     public static class DBHelper
     {
         public static string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=MVCWebAppTest;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        public static string newConnectionString = ConfigurationManager.ConnectionStrings["Default"].ConnectionString;
 
         public static int insertIntoStudents(StudentModel student)
         {
             int noOfRowsEffected;
             string sql = "INSERT INTO student (StudentID, FirstName, LastName, EmailAddress, Password) " +
                 $"VALUES ('{student.StudentID}','{student.FirstName}','{student.LastName}','{student.EmailAddress}','{student.Password}') ;";
-            SqlCommand cmd = new SqlCommand(sql, new SqlConnection(connectionString));
+            SqlCommand cmd = new SqlCommand(sql, new SqlConnection(newConnectionString));
             cmd.CommandType = System.Data.CommandType.Text;
             cmd.Connection.Open();
             noOfRowsEffected = cmd.ExecuteNonQuery();
@@ -29,7 +31,7 @@ namespace MVCWebApp.HelperClasses
         public static String GetStudent(string emailAddress, string password)
         {
             String sql = $"SELECT * FROM Student WHERE emailAddress = '{emailAddress}' AND password = '{password}' ;";
-            SqlCommand cmd = new SqlCommand(sql, new SqlConnection(connectionString));
+            SqlCommand cmd = new SqlCommand(sql, new SqlConnection(newConnectionString));
             cmd.CommandType = System.Data.CommandType.Text;
             cmd.Connection.Open();
             SqlDataReader sdr = cmd.ExecuteReader();
